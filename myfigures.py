785bbfd9b8ef8c591ee64aa945ee03598ec28d4f
Aline Freitas <alfreitas.ocn@gmail.com>
2023-05-07 09:12:21 +0100

    implementing my figures in publication quality

---
+++ myfigures.py
@@ -0,0 +1,20 @@
+"""
+Creating my personalized figures in article quality
+"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator,FormatStrFormatter,MaxNLocator
+
+def paper_fig():
+    mpl.rcParams['pdf.fonttype'] = 42
+    mpl.rcParams['ps.fonttype'] = 42
+    mpl.rcParams['font.family'] = 'Arial'
+
+    fig, axs = plt.subplots(figsize=(10,6), 
+                           dpi=300)
+    # This figure is positioned by its anchor point, 
+    # left bottom corner (0.1,0.1), and the size 
+    # parameters (0.5,0.8)
+    axs.tick_params(axis='both',labelsize=15)
+    plt.show()
+    return axs, fig
