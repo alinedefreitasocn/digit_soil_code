5ea91c061d2cf2b1bf0801a6382536987bbe2fcb
Aline de Freitas <alfreitas.ocn@gmail.com>
2023-05-16 07:21:33 +0100

    Delete running_plan_b.py

--- running_plan_b.py
+++
@@ -1,7 +0,0 @@
-import warming_model
-from warming_model import plan_b
-import matplotlib.pyplot as plt
-
-params = warming_model.params_enzyme_default
-
-df = warming_model.plan_b.temperature_regulating(params, warming_model.temp)
